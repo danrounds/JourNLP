78b61769c8d90f68837cda95f752995f03d70b9e
Dan Rounds <dan.rounds@gmail.com>
2017-01-24 21:28:31 -0500

    Clean-up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+app.use(express.static('public')); // /public now serves static files
 app.listen(process.env.PORT || 8080);
 
 module.exports = {app};
