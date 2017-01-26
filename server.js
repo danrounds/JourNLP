f6a79b47a00186bf1030ed6bb08a30e978480776
Dan Rounds <dan.rounds@gmail.com>
2017-01-26 18:50:35 -0500

    Mock out API calls for all entries and for individual-entries-by-id

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+const {Entries} = require('./API-fake.js');
+
+app.use(bodyParser.json());
+
 app.use(express.static('public')); // /public now serves static files
 app.listen(process.env.PORT || 8080);
 
+app.get('/api/entries', (req, res) => {
+    Entries
+        .find()
+        .exec()
+        .then(entries => res.json(entries));
+});
+
+app.get('/api/entries/:id', (req, res) => {
+    Entries
+        .findById(req.params.id)
+        .exec()
+        .then(entry => res.json(entry));
+});
+
 module.exports = {app};
+
