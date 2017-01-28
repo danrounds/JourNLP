439466ec6a33cc3b56009cc83863f4fad6b72300
Dan Rounds <dan.rounds@gmail.com>
2017-01-27 19:15:04 -0500

    Add barebones client script for write/edit-entry screen

--- public/view-and-write.js
+++ public/view-and-write.js
@@ -5,35 +5,33 @@ var state = {
     current: null
 };
 
-// &sidebar
-//   &sidebar-entry
-//     &entry-link
-//       date, excerpt, keywords
-//
-// &entry-display
-//   &title
-//   p (no class nor id -- might need one) -- our entry text
-//   button[edit]
-//
-// &tags-bar
-//   tags-text
-
-function getEntries() {
-    // returns a JSON array, from our API
-    return $.getJSON('../api/entries');
-}
+// function getEntries() {
+//     // returns a JSON array, from our API
+//     return $.getJSON('../api/entries');
+// }
 
 function populateState() {
     if (!state.entries.length) {
         // If our state isn't populated, we get data via API
-        return getEntries().
-            done(function(data) {
+        // return getEntries().
+        return $.getJSON('../api/entries')
+            .done(function(data) {
                 state.entries = data;
                 state.current = data[0];
             });
     }
     return $.Deferred().resolve(state);
     // lets us use the same interface, whether our data are async or synchronous
+}
+
+function submitEntry(data) {
+    return $.post(
+        { url: '../api/entries',
+          data: JSON.stringify(data),
+          dataType: 'json',
+          contentType: 'application/json'
+        }
+    );
 }
 
 function updateEntriesSidebar() {
@@ -61,7 +59,23 @@ function updateEntryView() {
 
 
 function updateWriteEditView() {
-    ;
+    $('button#discard').click(function(e) {
+        e.preventDefault();
+        if ($('#title-text').val().length !== 0 || $('#body-text').val().length !== 0) {
+            confirm('Are you sure you want to discard your entry?'); }
+    });
+    $('button#save').click(function(e) {
+        e.preventDefault();
+        var title = $('#title-text').val();
+        var body = $('#body-text').val()
+
+        if (title.length === 0) { alert('Your entry needs a title'); }
+        else if (body.length === 0) { alert('Your entry needs an actual body'); }
+        else {
+            submitEntry({title: title, body: body});
+            window.open(`view-entry.html?${state.current.id}`, '_self');
+        }
+    });
 }
 
 function viewEntryUpdate() {
