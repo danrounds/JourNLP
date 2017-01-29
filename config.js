3cd7dabe73d23f9d980adb30345bcf0816f6cbfd
Dan Rounds <dan.rounds@gmail.com>
2017-01-28 19:36:45 -0500

    Make server run from our (upcoming) tests

---
+++ config.js
@@ -0,0 +1,11 @@
+exports.DATABASE_URL =
+    process.env.DATABASE_URL ||
+    global.DATABASE_URL ||
+    'mongodb://localhost/journal-notes-app';
+
+exports.TEST_DATABASE_URL =
+    process.env.TEST_DATABASE_URL ||
+    'mongodb://localhost/test-journal-notes-app';
+
+exports.PORT = process.env.PORT
+    || 8080;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+
+const {DATABASE_URL, PORT} = require('./config');
 
 const {Entries} = require('./API-fake.js');
 
+
+const app = express();
 app.use(morgan('common'));
 app.use(bodyParser.json());
 
@@ -94,4 +98,47 @@ app.use('*', function(req, res) {
     res.status(404).json({message: 'Resource not found'});
 });
 
-module.exports = {app};
+
+// 
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Journal/Notes app is listening on port ${port}`);
+                resolve();
+            })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+// if we call server.js, directly (`node server.js'). this block runs
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+}
+
+// we export runServer and closeServer so that other code (right now, just
+// tests) can start/close the server
+module.exports = {runServer, closeServer, app};
