88434846c667598821959da762aa7f4e43a8c22b
Dan Rounds <dan.rounds@gmail.com>
2017-10-16 23:31:11 -0400

    Remove tons of redundant code from (pointless) static serving tests

--- test/statics.js
+++ test/statics.js
@@ -9,78 +9,25 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 describe('Pages', function() {
+    // In retrospect, testing whether or not static files serve is kind of a
+    // waste of time. Still kind of gratifying watching them pass, though.
+    function testEndpoint(getEndpoint) {
+        let res;
+        it('should return a 200 status code', () => chai.request(app)
+           .get(getEndpoint)
+           .then((res_) => {
+               res = res_;
+               res.should.have.status(200);
+           }));
+        it('should return html', () => res.should.be.html);
+    }
 
     before(() => runServer(TEST_DATABASE_URL));
-
     after(() => closeServer());
 
-    describe('index.html', function() {
-        let res;
-        it('should return a 200 status code', function() {
-            return chai.request(app)
-                .get('/')
-                .then((res_) => {
-                    res = res_;
-                    res.should.have.status(200) });
-        });
-        it('should return html', function() {
-            res.should.be.html;
-        });
-    });
-
-    describe('Text entry page (write-entry.html)', function() {
-        let res;
-        it('should return a 200 status code', function() {
-            return chai.request(app)
-                .get('/write-entry.html')
-                .then((res_) => {
-                    res = res_;
-                    res.should.have.status(200)});
-        });
-        it('should return html', function() {
-            res.should.be.html;
-        });
-    });
-
-    describe('Entry-view page (view-entry.html)', function() {
-        let res;
-        it('should return a 200 status code', function() {
-            return chai.request(app)
-                .get('/view-entry.html')
-                .then((res_) => {
-                    res = res_;
-                    res.should.have.status(200)});
-        });
-        it('should return html', function() {
-            res.should.be.html;
-        });
-    });
-
-    describe('Entries lists page', function() {
-        let res;
-        it('should return a 200 status code', function() {
-            return chai.request(app)
-                .get('/listings.html')
-                .then((res_) => {
-                    res = res_;
-                    res.should.have.status(200)});
-        });
-        it('should return html', function() {
-            res.should.be.html;
-        });
-    });
-
-    describe('Listings analysis page', function() {
-        let res;
-        it('should return a 200 status code', function() {
-            return chai.request(app)
-                .get('/analysis.html')
-                .then((res_) => {
-                    res = res_;
-                    res.should.have.status(200)});
-        });
-        it('should return html', function() {
-            res.should.be.html;
-        });
-    });
+    describe('Index', () => testEndpoint('/'));
+    describe('Text entry page (write-entry.html)', () => testEndpoint('/write-entry.html'));
+    describe('Entry-view page (view-entry.html)', () => testEndpoint('/view-entry.html'));
+    describe('Entries lists page (listings.html)', () => testEndpoint('/listings.html'));
+    describe('Listings analysis page (analysis.html)', () => testEndpoint('/analysis.html'));
 });
